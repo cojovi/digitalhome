6ea985feb719c5c7cc497f1367e08084165aad40
cojovi <31863818+cojovi@users.noreply.github.com>
2024-02-01 12:44:41 -0600

    bam

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,37 +1,29 @@
-import { ColorModeScript } from '@chakra-ui/react'
-import NextDocument, { Html, Head, Main, NextScript } from 'next/document'
-import theme from '../theme'
+import { ColorModeScript } from '@chakra-ui/react';
+import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
+import theme from '../theme';
 
-export default class Document extends NextDocument {
+export default class MyDocument extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href="/static/favicons/apple-touch-icon.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="32x32"
-            href="/static/favicons/favicon-32x32.png"
-          />
-          <link
-            rel="icon"
-            type="image/png"
-            sizes="16x16"
-            href="/static/favicons/favicon-16x16.png"
-          />
+          {/* Canonical link for the entire site; consider setting this at the page level instead for accuracy */}
+          <link rel="canonical" href="https://www.cojovi.com" />
+          
+          {/* Existing favicon and manifest links */}
+          <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/static/favicons/favicon-16x16.png" />
           <link rel="manifest" href="/static/favicons/manifest.json" />
+          
+          {/* Color mode script for Chakra UI */}
+          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         </Head>
         <body>
-          <ColorModeScript initialColorMode={theme.config.initialColorMode} />
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
