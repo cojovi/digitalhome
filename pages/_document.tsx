107bb1ec39ac4a72fec41ed042197c106bcd719f
cojovi <31863818+cojovi@users.noreply.github.com>
2024-02-02 15:01:53 -0600

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,11 +11,6 @@ export default class MyDocument extends NextDocument {
           {/* Canonical link for the entire site; consider setting this at the page level instead for accuracy */}
           <link rel="canonical" href="https://www.cojovi.com" />
           {/* Open Graph */}
-          <meta property="og:title" content={title} />
-          <meta property="og:description" content={description} />
-          <meta property="og:image" content={ogImage} />
-          <meta property="og:url" content={url} />
-          <meta property="og:type" content="website" />          
           {/* Existing favicon and manifest links */}
           <link rel="apple-touch-icon" sizes="76x76" href="/static/favicons/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/static/favicons/favicon-32x32.png" />
